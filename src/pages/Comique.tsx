fa00216045ec995383c54c456e418c394b72d44c
Salvak613 <jules.clauwaert@gmail.com>
2025-05-23 17:27:54 +0200

    coucou les loulous

--- src/pages/Comique.tsx
+++ src/pages/Comique.tsx
@@ -4,7 +4,7 @@ import Modal from "../components/Modal";
 import Password from "../components/Password";
 import { useCount } from "../context/CountContext";
 import styles from "../styles/Comique.module.css";
-import image from "/comique.jpg";
+import image from "/comique.png";
 import image2 from "/comique2.jpg";
 
 export const Comique = () => {
